24482dc12169603dce378581c0817bfe4844c875
Muamer Kukic <kukicako@gmail.com>
2020-03-27 09:39:40 -0700

    SPRINT MVP

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,7 +1,7 @@
 """CPU functionality."""
 
 import sys
-
+#OPCODES
 LDI = 0b10000010
 PRN = 0b01000111
 HLT = 0b00000001
@@ -11,17 +11,30 @@ PUSH = 0b01000101
 CALL = 0b01010000
 RET = 0b00010001
 ADD = 0b10100000
+CMP = 0b10100111
+JMP = 0b01010100
+JEQ = 0b01010101
+JNE = 0b01010110
 
 SP = 7
+
+
+#FLAGS 4 CMP LESS THAN /GREATER THAN/ EQQUAL TOO
+LT = 0b00000100
+GT = 0b00000010
+EQ = 0b00000001
 
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
+        # Program Counter, address of the currently executing instruction
         self.pc = 0
         self.reg = [0] * 8
         self.ram = [0] * 256
+        self.fl = 0
+        # BT cleans up code so u can reuse and not have to type it out errytime
         self.bt = {
             LDI: self.op_ldi,
             PRN: self.op_prn,
@@ -32,6 +45,10 @@ class CPU:
             PUSH: self.op_push,
             CALL: self.op_call,
             RET: self.op_ret,
+            CMP: self.op_cmp,
+            JMP: self.op_jmp,
+            JEQ: self.op_jeq,
+            JNE: self.op_jne
         }
     
     def op_ldi(self, operand_a, operand_b):
@@ -43,8 +60,8 @@ class CPU:
     def op_hlt(self, operand_a, operand_b):
         sys.exit(0)
 
-    # def op_add(self, operand_a, operand_b):
-    #     self.alu("ADD", operand_a, operand_b)
+    def op_add(self, operand_a, operand_b):
+        self.alu("ADD", operand_a, operand_b)
 
     def op_mul(self, operand_a, operand_b):
         self.alu("MUL", operand_a, operand_b)
@@ -56,16 +73,35 @@ class CPU:
         self.push_val(self.reg[operand_a])
 
     def op_call(self, operand_a, operand_b):
+        # Pushes next instruction to stack
         self.push_val(self.pc + 2)
+        # sets pc to address the subroutine
         self.pc = self.reg[operand_a]
-    # def op_ret(self, operand_a, operand_b):
-    #     self.pc = self.pop_val()
-    def op_ret(self):
-        self.pc = self.ram[self.reg[SP]]
-        self.reg[SP] += 1
-    def op_add(self):
-        self.alu("ADD", self.ram_read(self.pc + 1), self.ram_read(self.pc + 2))
-        self.pc += 3
+
+    def op_ret(self, operand_a, operand_b):
+        # poping next instruction of top of stack
+        self.pc = self.pop_val()
+
+    def op_cmp(self, operand_a, operand_b):
+        self.alu("CMP", operand_a, operand_b)
+
+    def op_jmp(self, operand_a, operand_b):
+        # Set the PC to the address stored in the given register
+        self.pc = self.reg[operand_a]
+
+    def op_jeq(self, operand_a, operand_b):
+        # If equal flag is set (true), jump to the address stored in the given register
+        if self.fl == EQ:
+            self.pc = self.reg[operand_a]
+        else:
+            self.pc += 2
+
+    def op_jne(self, operand_a, operand_b):
+        # If E flag is clear (false, 0), jump to the address stored in the given register
+        if not self.fl == EQ:
+            self.pc = self.reg[operand_a]
+        else:
+            self.pc += 2
 
     def load(self, filename):
         """Load a program into memory."""
@@ -93,10 +129,15 @@ class CPU:
 
 
     def ram_read(self, mar):
+        #accept addres to (mar) and return
+        #adress to write
+        #mar = memory adress register/ mdr = memory data register
         mdr = self.ram[mar]
         return mdr
 
     def ram_write(self, mdr, mar):
+        # Accept a value to write (mdr), and address
+        # to write it to (mar)
         self.ram[mar] = mdr
 
     def push_val(self, val):
@@ -116,6 +157,13 @@ class CPU:
             self.reg[reg_a] += self.reg[reg_b]
         elif op ==  "MUL":
             self.reg[reg_a] *= self.reg[reg_b]
+        elif op == "CMP":
+            if self.reg[reg_a] < self.reg[reg_b]:
+                self.fl = LT
+            elif self.reg[reg_a] > self.reg[reg_b]:
+                self.fl = GT
+            else:
+                self.fl = EQ
         #elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
@@ -128,7 +176,7 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
+            self.fl,
             #self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
@@ -145,11 +193,13 @@ class CPU:
         """Run the CPU."""
 
         while True:
+            #pulling instructions through memory
             instruction = self.ram[self.pc]
             operand_a = self.ram_read(self.pc + 1)
             operand_b = self.ram_read(self.pc + 2)
             instruction_length = (instruction >> 6) + 1
-            instruction_set_pc = (instruction >> 4) == 1
+            
+            instruction_set_pc = ((instruction >> 4) & 0b1) == 1
             if instruction in self.bt:
                 self.bt[instruction](operand_a, operand_b)
             #check if opcode instructions sets the pc
